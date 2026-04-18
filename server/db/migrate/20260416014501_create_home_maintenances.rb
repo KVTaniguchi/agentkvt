edbe1b6e82efb13e201e93186e83422f9f58e4a4
Kevin Taniguchi <kv.taniguchi@icloud.com>
2026-04-18 09:00:21 -0400

    Fix home maintenance workspace reference type

--- server/db/migrate/20260416014501_create_home_maintenances.rb
+++ server/db/migrate/20260416014501_create_home_maintenances.rb
@@ -1,7 +1,7 @@
 class CreateHomeMaintenances < ActiveRecord::Migration[8.0]
   def change
     create_table :home_maintenances do |t|
-      t.references :workspace, null: false, foreign_key: true
+      t.references :workspace, null: false, type: :uuid, foreign_key: true
       t.string :key_component, null: false
       t.datetime :last_serviced_at, null: false
       t.integer :standard_interval_days, null: false
